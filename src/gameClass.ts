9d9547662ab7b9b268238f7553e27d4055de4e82
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2022-04-30 18:04:06 +0200

    Able to create a grid, clear it

--- src/gameClass.ts
+++ src/gameClass.ts
@@ -1,9 +1,117 @@
+//Rows
+const alph: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+const emptyGrid: any[] = []
+
+
+
 class BattleshipsGame {
 
     numberOfShips: number
+    grid: any[]
 
+    //Create a 10x10 grid
     constructor() {
         this.numberOfShips = 0
+
+        for (let letter of alph) {
+            const row: { square: string, status: boolean | string, shipName: boolean | string }[] = []
+            for (let i = 1; i <= 10; i++) {
+                row.push({ square: `${letter}${i}`, status: false, shipName: false })
+            }
+            emptyGrid.push(row)
+        }
+        this.grid = emptyGrid
+    }
+
+    //Return the grid for testing purposes
+    returnGrid() {
+        return this.grid
+    }
+
+    //Clear the whole grid to starting position
+    clearGrid() {
+        for (let i = 0; i < 10; i++) {
+            for (let y = 0; y < 10; y++) {
+                this.grid[i][y].status = false;
+                this.grid[i][y].shipName = false
+            }
+        }
+    }
+
+    //Delete one ship from the grid
+    clearShip(ship: string) {
+        for (let i = 0; i < 10; i++) {
+            for (let y = 0; y < 10; y++) {
+                if (this.grid[i][y].shipName == ship) {
+                    this.grid[i][y].status = false;
+                    this.grid[i][y].shipName = false
+                }
+            }
+        }
+    }
+
+    //Function to create a ship. 2 options available 'battleship' and 'destroyer'
+    createShip = (shipName: string) => {
+        this.numberOfShips += 1
+        const rand: number = Math.floor(Math.random() * 2) + 1
+        let size: number = 0
+        let maxGridValue: number = 0
+        //The maximum grid values depending on the size of the ship
+        if (shipName.toLowerCase() == 'battleship') {
+            size = 5
+            maxGridValue = 5
+        } else if (shipName.toLowerCase() == 'destroyer') {
+            size = 4
+            maxGridValue = 6
+        } else {
+            console.log('Please enter a valid ship name')
+            return
+        }
+
+        if (rand === 1) {
+            //Create a horizontal ship
+            let column: number = Math.floor(Math.random() * 10)
+            let row: number = 10
+            while (row > maxGridValue) {
+                column = Math.floor(Math.random() * 10)
+                row = Math.floor(Math.random() * 10)
+
+                if (row <= maxGridValue) {
+                    for (let i = 0; i < size; i++) {
+                        //If a ship is already present, clear the current ship and start from scratch
+                        if (this.grid[column][row + i].status == true) {
+                            this.clearShip(`${shipName}${this.numberOfShips}`)
+                            row = 10
+                            break
+                        } else {
+                            this.grid[column][row + i].status = true
+                            this.grid[column][row + i].shipName = `${shipName}${this.numberOfShips}`
+                        }
+                    }
+                }
+            }
+        } else {
+            //Create a vertical ship
+            let column: number = 10
+            let row: number = Math.floor(Math.random() * 10)
+            while (column > maxGridValue) {
+                column = Math.floor(Math.random() * 10)
+                row = Math.floor(Math.random() * 10)
+
+                if (column <= maxGridValue) {
+                    for (let i = 0; i < size; i++) {
+                        if (this.grid[column + i][row].status == true) {
+                            this.clearShip(`${shipName}${this.numberOfShips}`)
+                            column = 10
+                            break
+                        } else {
+                            this.grid[column + i][row].status = true
+                            this.grid[column + i][row].shipName = `${shipName}${this.numberOfShips}`
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,32 @@
 const battleshipsGame = require('../src/gameClass')
 
-test('It should start', () => {
+test('The game should exist', () => {
     const battleships = new battleshipsGame
+})
+
+test('The game should create a grid to play on', () => {
+    const battleships = new battleshipsGame
+
+    expect(battleships.returnGrid())
+})
+
+test('We should be able to clear the grid to initial values', () => {
+    const battleships = new battleshipsGame
+    battleships.clearGrid()
+    const grid = battleships.returnGrid()
+
+    for (let i = 0; i < 10; i++) {
+        for (let y = 0; y < 10; y++) {
+            expect(grid[i][y].status).toBe(false)
+            expect(grid[i][y].shipName).toBe(false)
+        }
+    }
+})
+
+test('We should be able to create a battleship ship on the grid', () => {
+    const battleships = new battleshipsGame
+    battleships.createShip('battleship')
+    const grid = battleships.returnGrid()
+
+    console.log(grid)
 })
