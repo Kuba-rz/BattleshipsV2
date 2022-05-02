8c4732443911f9e6c05e5f7dddde598a6fda4d6e
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2022-05-02 14:19:46 +0200

    Add tests for adding the rest of the ships to the grid

--- test/app.test.ts
+++ test/app.test.ts
@@ -28,7 +28,66 @@ test('We should be able to create a battleship on the grid', () => {
     battleships.createShip('battleship')
     const grid = battleships.returnGrid()
 
-    console.log(grid)
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'battleship1' })])
+        ])
+    );
+})
+
+test('We should be able to create a destroyer on the grid', () => {
+    const battleships = new battleshipsGame
+    battleships.createShip('destroyer')
+    const grid = battleships.returnGrid()
+
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'destroyer1' })])
+        ])
+    );
+})
+
+test('We should be able to create 2 destroyers on the grid', () => {
+    const battleships = new battleshipsGame
+    battleships.createShip('destroyer')
+    battleships.createShip('destroyer')
+    const grid = battleships.returnGrid()
+
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'destroyer1' })])
+        ])
+    );
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'destroyer2' })])
+        ])
+    );
+})
+
+test('We should be able to create 2 destroyers and 1 battleship on the grid', () => {
+    const battleships = new battleshipsGame
+    battleships.createShip('destroyer')
+    battleships.createShip('destroyer')
+    battleships.createShip('battleship')
+    const grid = battleships.returnGrid()
+
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'destroyer1' })])
+        ])
+    );
+    expect(grid).toEqual(
+        expect.arrayContaining([
+            expect.arrayContaining([
+                expect.objectContaining({ shipName: 'destroyer2' })])
+        ])
+    );
 
     expect(grid).toEqual(
         expect.arrayContaining([
